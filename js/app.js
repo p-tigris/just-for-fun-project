367832eac2c00d7ab92a70bcb4196bce64cccb51
Shoumik Dhar <shoumik_dhar@yahoo.com>
2025-07-03 19:56:27 -0400

    Second commit

--- js/app.js
+++ js/app.js
@@ -1,46 +1,35 @@
 import { animeQuestions, disneyQuestions } from "./data.js";
-import { animeImageEl, animeButtonEl, disneyImageEl, disneyButtonEl, resetButtonEl } from "./setup.js";
+import { animeImageEl, animeButtonEl, disneyImageEl, disneyButtonEl, imageContainerEl, buttonContainerEl, choiceButtonsEl, resetButtonEl } from "./setup.js";
 
 let score;
 let currentQuestionIndex;
 
 const quizContainer = document.querySelector('#quiz-container');
-
-const imageContainer = document.createElement('div');
-const buttonContainer = document.createElement('div');
-
 const messageEl = document.querySelector('#message');
-
 
 
 const init = () => {
     quizContainer.innerHTML = "";
-    quizContainer.appendChild(imageContainer);
-    quizContainer.appendChild(buttonContainer);
-    imageContainer.style.display = "flex";
-    imageContainer.style.gap = "2em";
-    buttonContainer.style.display = "flex";
-    buttonContainer.style.justifyContent = "space-around";
-    buttonContainer.style.marginTop = "1em";
+    quizContainer.appendChild(imageContainerEl);
+    quizContainer.appendChild(buttonContainerEl);
     messageEl.textContent = "Choose your quiz!";
     score = 0;
     currentQuestionIndex = 0;
-    imageContainer.appendChild(animeImageEl);
-    imageContainer.appendChild(disneyImageEl);
-    buttonContainer.appendChild(animeButtonEl);
-    buttonContainer.appendChild(disneyButtonEl);
+    imageContainerEl.appendChild(animeImageEl);
+    imageContainerEl.appendChild(disneyImageEl);
+    buttonContainerEl.appendChild(animeButtonEl);
+    buttonContainerEl.appendChild(disneyButtonEl);
+    imageContainerEl.style.display = "flex";
+    imageContainerEl.style.gap = "2em";
+    buttonContainerEl.style.display = "flex";
+    buttonContainerEl.style.justifyContent = "space-around";
+    buttonContainerEl.style.marginTop = "1em";
 }
 
 document.addEventListener('DOMContentLoaded', init)
-
-const initAnimeQuiz = () => {
-    init();
-    render();
-}
     
 const render = () => {
     quizContainer.innerHTML = "";
-    quizContainer.style.display = "flex";
     messageEl.textContent = "What anime is this?"
     const quizImage = document.createElement('img');
     quizImage.src = animeQuestions[currentQuestionIndex].image;
@@ -48,10 +37,13 @@ const render = () => {
     quizImage.height = 400;
     quizImage.width = 500;
     quizContainer.appendChild(quizImage);
+    quizImage.style.display = "flex";
+    choiceButtonsEl.innerHTML = "";
+    quizContainer.appendChild(choiceButtonsEl);
     animeQuestions[currentQuestionIndex].choices.forEach((choice) => {
         const choiceButton = document.createElement('button');
         choiceButton.textContent = choice;
-        quizContainer.appendChild(choiceButton);
+        choiceButtonsEl.appendChild(choiceButton);
         choiceButton.addEventListener('click', checkAnswer)
         choiceButton.addEventListener('click', next)
     })
@@ -81,7 +73,7 @@ const next = () => {
             finalMessage();
             quizContainer.appendChild(resetButtonEl);
         }
-})
+    })
 }
 
 const finalMessage = () => {
@@ -104,13 +96,3 @@ const finalMessage = () => {
 resetButtonEl.addEventListener('click', init);
 
 animeButtonEl.addEventListener('click', render);
-
-const initDisneyQuiz = () => {
-    init();
-
-    disneyImage.height = 500;
-    disneyImage.width = 500;
-
-    messageEl.innerHTML = "Guess the Disney lyrics!";
-    
-}
--- js/setup.js
+++ js/setup.js
@@ -15,6 +15,19 @@ animeButtonEl.textContent = "Guess the Anime"
 export const disneyButtonEl = document.createElement('button');
 disneyButtonEl.textContent = "Guess the Disney Lyrics";
 
+// Containers to hold the above images and buttons
+export const imageContainerEl = document.createElement('div');
+export const buttonContainerEl = document.createElement('div');
+
+// Choice buttons for each quiz question
+export const choiceButtonsEl = document.createElement('div');
+choiceButtonsEl.style.display = "grid";
+choiceButtonsEl.style.justifyContent = "center";
+choiceButtonsEl.style.alignItems = "center";
+choiceButtonsEl.style.gridTemplateColumns = "repeat(2, 200px)";
+choiceButtonsEl.style.gap = "10px";
+choiceButtonsEl.style.marginTop = "20px";
+
 // Reset button for end of quiz
 export const resetButtonEl = document.createElement('button');
 resetButtonEl.textContent = "Return to Home Page";
